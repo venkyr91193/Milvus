1acd0314dd7a4681312d40b7a58b1f5bc72f6240
Venkataramana Radhakrishnan <venkat@Venkats-MacBook-Pro.local>
2021-08-20 14:27:41 +0530

    fix(similarity): delete collection when exiting

--- tutorials/similarity_examples.py
+++ tutorials/similarity_examples.py
@@ -20,6 +20,7 @@ def modelling_example():
     sentences = list(sentences_index.values())
     # dimension should be 1024, can be changed in the tools_milvus file according to the model used
     vectors = model_obj.make_sentence_vectors(sentences)
+    check_and_add_collection('example_collection', 1024)
     add_vectors_to_milvus('example_collection', vectors, ids)
 
     # query the most similar sentence
@@ -28,7 +29,10 @@ def modelling_example():
     result = query_milvus('example_collection', new_vector)
     for index,res in enumerate(result.id_array[0]):
         print(f"Sentence '{sentences_index.get(res)}' is similar by {result.distance_array[0][index]}")
-
+    
+    # delete the collection when existing if you want to run the code again with different examples
+    delete_collection('example_collection')
+    
 if __name__ == "__main__":
     modelling_example()
     print("All tests passed")
